879f3807a02ef2ee120c8531aea8323d4c4bdf5f
Jaudat Syed <sjh.jaudat@gmail.com>
2013-02-07 01:14:07 -0500

    problem with mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Utcal::Application.config.secret_token = '24c6200edc8aaadd0e51b92c426811649bfcc5d2e5dee7524f435ab832f54efd78101471913c538973ff9dbaecd20fbc361ec009941dca995437a0a9fc36f173'
