c74b0c710b60cb2ca0844396d1e51a3ba8899c6f
Jaudat Syed <sjh.jaudat@gmail.com>
2013-02-28 06:04:15 -0500

    created signup page for student and linked it to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Utcal::Application.routes.draw do
 
-  root to: 'static_pages#home' 
+  root to: 'static_pages#home'
+  match '/st_signup',  to: 'students#new'
+  match '/prof_signup',  to: 'professors#new'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
