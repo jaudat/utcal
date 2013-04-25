6d590316c7ceeff9c2bb7c495ef665c04082dab5
Jaudat Syed <sjh.jaudat@gmail.com>
2013-04-24 22:57:15 -0400

    redirecting from homepage to either students or profs

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
+  	if session[:type] == "Student"
+  		student = Student.find_by_user_id(session[:remember_token])
+  		redirect_to student
+  	elsif session[:type] == "Professor"
+  		professor = Professor.find_by_user_id(session[:remember_token])
+  		redirect_to professor
+  	end
   end
 
   def help
