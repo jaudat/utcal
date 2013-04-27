6588ab33fc5daa4a3915426f654a3ca486c27a01
Zieny <zieny@Waterfall.(none)>
2013-04-27 18:35:08 -0400

    remove header strings

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-<<<<<<< HEAD
-end
-=======
+
   	if session[:type] == "Student"
   		student = Student.find_by_user_id(session[:remember_token])
   		redirect_to student
@@ -11,7 +9,6 @@ end
   		redirect_to professor
   	end
   end
->>>>>>> 6d590316c7ceeff9c2bb7c495ef665c04082dab5
 
   def help
   end
